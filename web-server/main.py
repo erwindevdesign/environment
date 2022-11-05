698d85bdbac9241238e6654a95dea2ae0b0ad20b
erwindevdesign <erwinf.sanchez@outlook.com>
2022-11-05 06:54:38 -0600

    Add Dockers env

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,6 @@
 import store
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,9 +8,26 @@ app = FastAPI()
 def get_list():
     return[1,2,3]
 
-@app.get('/contact')
+@app.get('/contact', response_class=HTMLResponse)
 def get_list():
-    return{'name' : 'Platzi'}
+    """ return{'name' : 'Platzi'} """
+    return """ 
+    
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Title desde main.py</h1>
+            <p> I a text in p </p>
+        </body>
+        </html>
+
+     """
 
 def run():
     store.get_categories()
